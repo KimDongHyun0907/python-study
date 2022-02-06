8b536aa99e051c72951ed18d507a73eeb1f0e238
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2022-02-06 23:36:41 +0900

    Add files via upload

---
+++ arithmetic operation.py
@@ -0,0 +1,9 @@
+"""
+두 수의 사칙연산 출력
+"""
+a,b=map(int,input().split())
+print(f'덧셈 {a}+{b}={a+b}')
+print(f'뺄셈 {a}-{b}={a-b}')
+print(f'곱셈 {a}*{b}={a*b}')
+print(f'나눗셈 {a}/{b}={a//b}')
+print(f'나머지 {a}%{b}={a%b}')

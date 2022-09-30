78b78071f8522973eee0db32dd86a0354da258fa
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2022-09-30 23:56:08 +0900

    Create slicing.py

---
+++ list/slicing.py
@@ -0,0 +1,15 @@
+arr=[0,1,2,3,4,5]
+
+print(arr[1:4:2])
+# 1번 index에서 3번 index까지 2씩 증가 출력
+# arr[1], arr[3]
+
+print(arr[1:4])
+print(arr[:4])
+print(arr[4:])
+
+print(arr[4:1:-1])
+# 4번 index에서 2번 index까지 1씩 감소 출력
+# arr[4], arr[3], arr[2]
+
+print(arr[::-1])

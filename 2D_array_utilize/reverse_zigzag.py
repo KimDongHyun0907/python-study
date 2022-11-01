e00a6580f81d6f89ba5c419b27ac40592a40ebc9
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2022-11-01 23:57:25 +0900

    Update reverse_zigzag.py

--- 2D_array_utilize/reverse_zigzag.py
+++ 2D_array_utilize/reverse_zigzag.py
@@ -2,16 +2,18 @@ n,m=map(int,input().split())
 
 arr=[[0 for _ in range(m)] for _ in range(n)]
 
-cnt=1
+cnt,num=1,1
+
 for i in range(m-1,-1,-1):
-    if i%2==1:
+    if cnt%2==1:
         for j in range(n-1,-1,-1):
-            arr[j][i]=cnt
-            cnt+=1
+            arr[j][i]=num
+            num+=1
     else:
         for j in range(n):
-            arr[j][i]=cnt
-            cnt+=1
+            arr[j][i]=num
+            num+=1
+    cnt+=1
 
 for i in arr:
     for j in i:

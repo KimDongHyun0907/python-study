71c0d3e84efbfaaa3fbc832118b943748683f89f
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2022-11-10 20:56:43 +0900

    Create lower_upper.py

---
+++ string_rotate_ascii/lower_upper.py
@@ -0,0 +1,11 @@
+x='H'
+print(ord(x))  # 72
+print(ord(x)-ord('A'))  # 7
+print(ord(x)-ord('A')+ord('a'))  # x의 소문자 아스키 번호 = 104
+print(chr(ord(x)-ord('A')+ord('a')))  # 아스키 번호를 문자로 변환 h
+
+-----------------------------------------------------------------------
+
+x=input()
+
+print(chr(ord(x)-ord('a')+ord('A')))

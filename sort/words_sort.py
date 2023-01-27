c18138831c6846093ea26785b6aa945f9d5e54a2
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-01-27 23:57:55 +0900

    Create words_sort.py

---
+++ sort/words_sort.py
@@ -0,0 +1,15 @@
+words=['computer','bed','banana','pencil']
+words.sort()
+print(words) # ['banana', 'bed', 'computer', 'pencil']
+
+words=['computer','bed','banana','pencil']
+words.sort(reverse=True)
+print(words) # ['pencil', 'computer', 'bed', 'banana']
+
+words=['computer','bed','banana','pencil']
+words=sorted(words)
+print(words) # ['banana', 'bed', 'computer', 'pencil']
+
+words=['computer','bed','banana','pencil']
+words=sorted(words,reverse=True)
+print(words) # ['pencil', 'computer', 'bed', 'banana']

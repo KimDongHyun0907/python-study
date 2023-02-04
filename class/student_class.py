ecb38d74debf3af895bc46170171a99e90f6fdef
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-02-04 23:55:12 +0900

    Create student_class.py

---
+++ class/student_class.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self,kor,math,eng):
+        self.kor=kor
+        self.math=math
+        self.eng=eng
+
+student1=Student(50,60,70)
+print(student1.kor)
+print(student1.math)
+print(student1.eng)

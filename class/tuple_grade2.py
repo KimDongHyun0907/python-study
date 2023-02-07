7fc13a5397fd57faf8a8d9217cdf9ab03d6fc2fd
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-02-07 19:01:34 +0900

    Create tuple_grade2.py

---
+++ class/tuple_grade2.py
@@ -0,0 +1,11 @@
+student=(0,0,0)
+kor,math,eng=student
+print(f'학생의 국어 성적은 {kor}')
+print(f'학생의 수학 성적은 {math}')
+print(f'학생의 영어 성적은 {eng}')
+
+student=(kor,80,90)
+kor,math,eng=student
+print(f'학생의 국어 성적은 {kor}')
+print(f'학생의 수학 성적은 {math}')
+print(f'학생의 영어 성적은 {eng}')

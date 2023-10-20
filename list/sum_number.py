f18a92dc3d6172f8e9cd6747e47eec9ab211c170
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-10-20 22:55:15 +0900

    Delete list directory

--- list/sum_number.py
+++
@@ -1,5 +0,0 @@
-arr=input().split()
-sum_val=0
-for i in arr:
-    sum_val+=int(i)
-print(sum_val)
--- list/use_map_function.py
+++
@@ -1,7 +0,0 @@
-arr=list(map(int,input().split()))
-print(arr)
-
-a,b,c=list(map(int,input().split()))
-print(b)
-print(c)
-print(a)

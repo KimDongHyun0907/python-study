b394fb45b8b9c048d8bb79ba1728f677fbce28b8
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-10-20 22:56:05 +0900

    Delete list_search directory

--- list_search/count_value_count().py
+++
@@ -1,4 +0,0 @@
-word=['a','b','a','b','b']
-val=input()
-
-print(word.count(val))
--- list_search/count_value_for_loop.py
+++
@@ -1,9 +0,0 @@
-word=['a','b','a','b','b']
-val=input()
-cnt=0
-
-for i in word:
-    if i==val:
-        cnt+=1
-
-print(f'{val}의 개수는 {cnt}')

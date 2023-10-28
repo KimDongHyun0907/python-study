71a3eabcf3e23d47966474de9deaded9a00b034a
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-10-28 16:50:38 +0900

    Update black_and_white.py

--- specific_section/black_and_white.py
+++ specific_section/black_and_white.py
@@ -1,39 +1,49 @@
-# 미완성
-
 import sys
 
 n = int(input())
-distance_arr = []
-point = 0
-min_point = sys.maxsize
-max_point = -sys.maxsize
+curr = 0
+max_size = -sys.maxsize
+min_size = sys.maxsize
+arr = []
 
 for _ in range(n):
-    distance, direction = input().split()
-    distance = int(distance)
-    if direction == 'R':
-        distance_arr.append([point, point+distance, direction])
-        point += distance
-        max_point = max(point, max_point)
+    a, b = input().split()
+    a = int(a)
+    arr.append((a, b))
+    if b == 'R':
+        max_size = max(max_size, curr+a-1)
+        curr += a-1
     else:
-        distance_arr.append([point-distance, point, direction])
-        point -= distance
-        min_point = min(point, min_point)
+        min_size = min(min_size, curr-a+1)
+        curr -= a-1
 
-if min_point < 0:
-    offset = min_point*(-1)
-    max_point += offset
-    distance_arr = list(map(lambda x: [x[0]+offset, x[1]+offset, x[2]], distance_arr))
+curr = 0
+if min_size < 0:
+    max_size += abs(min_size)
+    curr = abs(min_size)
 
-color_check = [[0, 0, 0] for _ in range(max_point+1)]  # w, g, b
+color_check = [[0, 0, 0] for _ in range(max_size+1)]
 
-for x, y, z in distance_arr:
-    if z == 'R':
-        for i in range(x, y+1):
+for x, y in arr:
+    if y == 'R':
+        for i in range(curr, curr+x):
+            color_check[i][1] += 1
+            color_check[i][2] = 'b'
+        curr += x-1
+    else:
+        for i in range(curr-x+1, curr+1):
             color_check[i][0] += 1
-    else:
-        for i in range(x, y+1):
-            color_check[i][2] += 1
+            color_check[i][2] = 'w'
+        curr -= x-1
 
-print(color_check)
-print(distance_arr)
+white, black, gray = 0, 0, 0
+for x, y, z in color_check:
+    if x >= 2 and y >= 2:
+        gray += 1
+    else:
+        if z == 'b':
+            black += 1
+        else:
+            white += 1
+
+print(white, black, gray)

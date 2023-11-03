447e94b1d739a8ff8f93aa69d4191b9ac051caa7
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-11-03 23:21:18 +0900

    Update and rename non_overlapping_square.py to obstacle_remove_area.py

---
+++ square_problem/obstacle_remove_area.py
@@ -0,0 +1,39 @@
+import sys
+
+n = int(input())  # 사각형 개수
+
+min_x, min_y = sys.maxsize, sys.maxsize  # n개의 사각형 중 x 좌표와 y 좌표의 최솟값
+max_x, max_y = -sys.maxsize, -sys.maxsize  # n개의 사각형 중 x 좌표와 y 좌표의 최솟값
+arr = []  # (x1, y1, x2, y2) 형식의 원소를 가지는 배열 (리스트)
+offset_x, offset_y = 0, 0  # 좌표가 음수가 나올 수 있으므로 offset 설정
+
+for _ in range(n):
+    x1, y1, x2, y2 = map(int, input().split())
+    min_x, min_y = min(x1, min_x), min(y1, min_y)
+    max_x, max_y = max(x2, max_x), max(y2, max_y)
+    arr.append((x1, y1, x2, y2))
+
+obstacle = tuple(map(int, input('장애물의 사각형은? ').split()))
+
+if min_x < 0:
+    max_x += abs(min_x)
+    offset_x += abs(min_x)
+if min_y < 0:
+    max_y += abs(min_y)
+    offset_y += abs(min_y)
+
+checked = [[False for _ in range(max_y+1)] for _ in range(max_x+1)]
+cnt = 0  # 직사각형 넓이
+for x1, y1, x2, y2 in arr:
+    for i in range(x1+offset_x, x2+offset_x):
+        for j in range(y1+offset_y, y2+offset_y):
+            if not checked[i][j]:
+                checked[i][j] = True
+                cnt += 1
+
+xa, ya, xb, yb = obstacle
+for i in range(xa+offset_x, xb+offset_x):
+    for j in range(ya+offset_y, yb+offset_y):
+        if checked[i][j]:
+            cnt -= 1
+print(cnt)

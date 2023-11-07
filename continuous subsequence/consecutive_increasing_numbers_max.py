b1693c8d127204ee94d8d993e246b9fc1de99e1b
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-11-07 23:55:18 +0900

    Create consecutive_increasing_numbers_max.py

---
+++ continuous subsequence/consecutive_increasing_numbers_max.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(map(int, input().split()))
+max_cnt = 0
+max_arr = []
+for i in range(n):
+    if i == 0 or arr[i-1] >= arr[i]:
+        cnt = 1
+        temp_arr = [arr[i]]
+    else:
+        cnt += 1
+        temp_arr.append(arr[i])
+    max_cnt = max(cnt, max_cnt)
+    if len(temp_arr) > len(max_arr):
+        max_arr = temp_arr
+print(max_cnt)
+print(max_arr)

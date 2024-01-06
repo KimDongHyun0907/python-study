779e6c077dbbba9a0f68ff53a4e9017412edcfc0
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-01-06 23:47:12 +0900

    Update moving_in_direction.py

--- dx dy/moving_in_direction.py
+++ dx dy/moving_in_direction.py
@@ -1,19 +1,11 @@
-n=int(input())
-x,y=0,0
-dir=[(-1,0), (1,0), (0,1), (0,-1)]
-for _ in range(n):
-    d, m = input().split()
-    m=int(m)
-    if d=='W':
-        dnum=0
-    elif d=='E':
-        dnum=1
-    elif d=='N':
-        dnum=2
-    else:
-        dnum=3
-    
-    x+=dir[dnum][0]*m
-    y+=dir[dnum][1]*m
+n = int(input())
+x, y = map(int, input().split())
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+directions = {'N': 0, 'E': 1, 'S': 2, 'W': 3}
 
-print(x,y)
+for _ in range(n):
+    d, distance = input().split()
+    distance = int(distance)
+    x, y = x+dx[directions[d]]*distance, y+dy[directions[d]]*distance
+
+print(x, y)

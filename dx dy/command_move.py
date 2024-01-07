7759dcec2702c56f820a36eedd087a7533ab50d3
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-01-07 20:30:44 +0900

    Update command_move.py

--- dx dy/command_move.py
+++ dx dy/command_move.py
@@ -1,14 +1,16 @@
-s=input()
-x,y=0,0
-dir=[(0,1),(1,0),(0,-1),(-1,0)]
-dir_num=0
-for val in s:
-    if val=='L':
-        dir_num=(dir_num-1)%4
-    elif val=='R':
-        dir_num=(dir_num+1)%4
+x, y = map(int, input('초기 위치: ').split())
+curr_d = input('방향 입력: ')
+ds = {'N': 0, 'E': 1, 'S': 2, 'W': 3}
+d = ds[curr_d]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+inp_ds = input()
+for val in inp_ds:
+    if val == 'L':
+        d = (d-1+4) % 4
+    elif val == 'R':
+        d = (d+1) % 4
     else:
-        x+=dir[dir_num][0]
-        y+=dir[dir_num][1]
+        x, y = x+dx[d], y+dy[d]
 
 print(x, y)

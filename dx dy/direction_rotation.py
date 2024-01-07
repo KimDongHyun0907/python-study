9f1b4e436b927ff055bfbfe9f26c6508845e65a5
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-01-07 21:30:30 +0900

    Create direction_rotation.py

---
+++ dx dy/direction_rotation.py
@@ -0,0 +1,14 @@
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+d_num = 0
+x, y = 0, 0
+
+print(f'{x+dx[d_num]}, {y+dy[d_num]}')
+
+d_num += 1
+print(f'{x+dx[d_num]}, {y+dy[d_num]}')
+
+d_num += 1
+print(f'{x+dx[d_num]}, {y+dy[d_num]}')
+
+d_num += 1
+print(f'{x+dx[d_num]}, {y+dy[d_num]}')

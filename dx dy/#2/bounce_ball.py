7c3b33f31dc1ddbb99c9e1731190acc1660e65b0
KimDongHyun <ehdgus8397@naver.com>
2024-10-23 12:59:04 +0900

    바운스 볼 최종 풀이

--- dx dy/#2/bounce_ball.py
+++ dx dy/#2/bounce_ball.py
@@ -1,64 +1,26 @@
-def in_range(x,y):
-  return 0<=x<n and 0<=y<n
+n = int(input())
+arr = [list(input()) for _ in range(n)]
+k = int(input())
 
-n=int(input())
-arr=[list(input()) for _ in range(n)]
-k=int(input())
+d = [(1, 0), (0, -1), (-1, 0), (0, 1)] # 아래 왼 위 오른
 
-if (k-1)//n==0:
-  x,y=0,k-1
-elif (k-1)//n==1:
-  x,y=(k-1)%n, n-1
-elif (k-1)//n==2:
-  x,y=n-1, (n-1)-(k-1)%n
-else:
-  x,y=(n-1)-(k-1)%n, 0
+d_num = (k - 1) // n
+x, y = [(0, k - 1), 
+            (k - n - 1, n - 1), 
+            (n - 1, (n - 1) - (k - 1) % n), 
+            ((n - 1) - (k - 1) % n, 0)][d_num]
 
-# x,y=0,1
-d = [(1,0),(0,1),(-1,0),(0,-1)] # 하 우 상 좌
-dir_num=0
-cnt=0
-
-while True:
-  if arr[x][y]=='\\':
-    dir_num=3-dir_num
-    dx,dy=d[dir_num]
-    x,y=x-dx,y-dy
+cnt = 0
+while 0 <= x < n and 0 <= y < n:
+  if arr[x][y] == '\\':
+    d_num = 3 - d_num
 
   else:
-    dir_num = (dir_num+1)%4
-    dx,dy=d[dir_num]
-    x,y=x-dx,y-dy
-  print(dir_num,x,y)
-  if not in_range(x,y):
-    print(cnt)
-    break
+    d_num = d_num ^ 1
   
-  cnt+=1
-  
+  dx, dy = d[d_num]
+  x, y = x + dx, y + dy
 
-'''
-\
-0 3
-1 2
-2 1
-3 0
+  cnt += 1
 
-/
-0 1
-1 2
-2 3
-3 0
-
-3
-/\\
-\\\
-/\/
-2
-
-0 01
-3 02
-0 12
-3 13
-
-'''
+print(cnt)

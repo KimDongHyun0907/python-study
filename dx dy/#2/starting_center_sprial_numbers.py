300c4f83be0f6ac575c42e49753b520578e032fe
KimDongHyun <ehdgus8397@naver.com>
2024-11-01 19:02:05 +0900

    가운데부터 나선형으로 숫자 채우기

---
+++ dx dy/#2/starting_center_sprial_numbers.py
@@ -0,0 +1,35 @@
+n = int(input())
+arr = [[0] * n for _ in range(n)]
+
+x, y = n // 2, n // 2
+arr[x][y] = 1
+d = [(0, 1), (-1, 0), (0, -1), (1, 0)] # 오른 위 왼 아래
+d_num, step, number = 0, 1, 2
+
+while number <= n * n:
+  for _ in range(2):
+    for _ in range(step):
+      if number > n * n:
+        break
+      dx, dy = d[d_num]
+      x, y = x + dx, y + dy
+      arr[x][y] = number
+      number += 1
+    d_num = (d_num + 1) % 4
+  step += 1
+
+for row in arr:
+  print(*row)
+
+
+'''
+n=3
+11 R 12 U 02 L 01 L 00 D 10 D 20 R 21 R 22
+RU LLDD RR
+
+n=5
+22 R 23 U 13 L 12 L 11 D 21 D 31 R 32 R 33 R 34 U 24 U 14 U
+04 L 03 L 02 L 01 L 00 D 10 D 20 D 30 D 40 R 41 R 42 R 43 R 44
+RU LLDD RRRUUU LLLLDDDD RRRR
+
+'''
